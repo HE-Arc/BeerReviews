95094c2a9cfcd3e7b2328e078e9a4b6a2262ec95
Guillaume Noguera <guillaume.noguera@gmail.com>
2018-02-26 11:25:14 +0100

    fixed models

--- beerreviews/beerreviewsapp/models.py
+++ beerreviews/beerreviewsapp/models.py
@@ -1,22 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 DEFAULT_LENGTH = 100
-
-class Beer(models.Model):
-    """ What did you expect ? """
-    name = models.CharField(max_length = DEFAULT_LENGTH)
-    description = models.TextField()
-    ebc = models.IntegerField()
-    style = models.ForeignKey(Style, on_delete = models.CASCADE)
-    maker = models.ForeignKey(Maker, on_delete = models.CASCADE)
-    country = models.ForeignKey(Country, on_delete = models.CASCADE)
-
-class Review(models.Model):
-    rating = models.IntegerField()
-    content = models.TextField()
-    date = models.DateField()
-    user = models.ForeignKey(User)
-    beer = models.ForeignKey(Beer)
 
 class Country(models.Model):
     name = models.CharField(max_length = DEFAULT_LENGTH)
@@ -26,3 +11,21 @@ class Style(models.Model):
 
 class Maker(models.Model):
     name = models.CharField(max_length = DEFAULT_LENGTH)
+
+class Beer(models.Model):
+    """ What did you expect ? """
+    name = models.CharField(max_length = DEFAULT_LENGTH)
+    description = models.TextField()
+    ebc = models.IntegerField()
+    style = models.ForeignKey(Style, on_delete = models.CASCADE)
+    maker = models.ForeignKey(Maker, on_delete = models.CASCADE)
+    country = models.ForeignKey(Country, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    rating = models.IntegerField()
+    content = models.TextField()
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    beer = models.ForeignKey(Beer, on_delete = models.CASCADE)
