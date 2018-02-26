80d5c63b1c1c03cab9d510b51e0d6ee307d64c7f
Guillaume Noguera <guillaume.noguera@gmail.com>
2018-02-26 10:51:07 +0100

    hello world + db

--- beerreviews/beerreviewsapp/models.py
+++ beerreviews/beerreviewsapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+DEFAULT_LENGTH = 100
+
+class Beer(models.Model):
+    """ What did you expect ? """
+    name = models.CharField(max_length = DEFAULT_LENGTH)
+    description = models.TextField()
+    ebc = models.IntegerField()
+    style = models.ForeignKey(Style, on_delete = models.CASCADE)
+    maker = models.ForeignKey(Maker, on_delete = models.CASCADE)
+    country = models.ForeignKey(Country, on_delete = models.CASCADE)
+
+class Review(models.Model):
+    rating = models.IntegerField()
+    content = models.TextField()
+    date = models.DateField()
+    user = models.ForeignKey(User)
+    beer = models.ForeignKey(Beer)
+
+class Country(models.Model):
+    name = models.CharField(max_length = DEFAULT_LENGTH)
+
+class Style(models.Model):
+    name = models.CharField(max_length = DEFAULT_LENGTH)
+
+class Maker(models.Model):
+    name = models.CharField(max_length = DEFAULT_LENGTH)
