8f6442eecee17ea4e65f437ef3b67e8002ae00a3
Guillaume Noguera <guillaume.noguera@gmail.com>
2018-02-26 19:54:29 +0100

    added js fadein and removed splash screen

--- beerreviews/beerreviewsapp/static/scr/main.js
+++ beerreviews/beerreviewsapp/static/scr/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
+        let fadeInTime = 500
         // create sidebar and attach to menu open
         $('.ui.sidebar')
           .sidebar('attach events', '.toc.item')
         ;
+        $('.beer-image').hide();
+        $('.beer-image').fadeIn(fadeInTime);
 });
