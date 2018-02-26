e4e40aa651997b4301bdfc651c00e6f95983cf0f
Guillaume Noguera <guillaume.noguera@gmail.com>
2018-02-26 19:37:16 +0100

    pas taper c'était la guerre

--- beerreviews/beerreviewsapp/models.py
+++ beerreviews/beerreviewsapp/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 DEFAULT_LENGTH = 100
 URL_LENGTH = 255
-
-class CountryManager(models.Manager):
-    """ Used by fixtures to resolve natural keys """
-    def get_by_natural_key(self, name):
-        return [self.get(name = name)]
 
 class StyleManager(models.Manager):
     """ Used by fixtures to resolve natural keys """
@@ -19,20 +15,11 @@ class MakerManager(models.Manager):
     def get_by_natural_key(self, name):
         return [self.get(name = name)]
 
-class Country(models.Model):
-    name = models.CharField(max_length = DEFAULT_LENGTH)
-    class Meta:
-        unique_together = ['name']
-
 class Style(models.Model):
     name = models.CharField(max_length = DEFAULT_LENGTH)
-    class Meta:
-        unique_together = ['name']
 
 class Maker(models.Model):
     name = models.CharField(max_length = DEFAULT_LENGTH)
-    class Meta:
-        unique_together = ['name']
 
 class Beer(models.Model):
     """ What did you expect ? """
@@ -41,10 +28,8 @@ class Beer(models.Model):
     ebc = models.IntegerField()
     style = models.ForeignKey(Style, on_delete = models.CASCADE)
     maker = models.ForeignKey(Maker, on_delete = models.CASCADE)
-    country = models.ForeignKey(Country, on_delete = models.CASCADE)
+    country = CountryField(blank_label='(select country)')
     imgsrc = models.CharField(max_length = URL_LENGTH, default = "https://cdn4.iconfinder.com/data/icons/proglyphs-food/512/Beer-512.png")
-    def getMaker():
-        return self.maker
     def __str__(self):
         return self.name
 
