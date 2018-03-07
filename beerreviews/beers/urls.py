d802c5ffae8187056a092dc400b6d09f34472eaa
Rieben Axel <axel.rieben@he-arc.ch>
2018-03-07 22:44:27 +0100

    Basic review creation (need validation)

--- beerreviews/beers/urls.py
+++ beerreviews/beers/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'beers'
 urlpatterns = [
     path('', views.BeerListView.as_view(), name="beers"),
-    path('<int:pk>', views.BeerDetailView.as_view(), name="detail"),
+    path('<int:pk>/', views.BeerDetailView.as_view(), name="detail"),
+    path('<int:beer_id>/review/create/', views.create_review, name="create_review")
 ]
--- beerreviews/beers/views.py
+++ beerreviews/beers/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views import generic
 
-from .models import Beer
+from .models import Beer, Review
+
 
 ### Beers
 
@@ -18,3 +21,14 @@ class BeerDetailView(generic.DetailView):
 
 
 ### Reviews
+def create_review(request, beer_id):
+    beer = get_object_or_404(Beer, pk=beer_id)
+    if request.user.is_authenticated:
+        review = Review(rating=request.POST['rating'], content=request.POST['comment'], beer=beer, user=request.user)
+        review.save()
+    else:
+        return render(request, 'beers/beer_detail.html', {
+            'beer': beer,
+            'error_message': "You need to be logged in to rate a beer",
+        })
+    return HttpResponseRedirect(reverse('beers:detail', args=(beer.id,)))
