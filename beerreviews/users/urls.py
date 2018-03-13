596bb1b5da7c9678a0c6adfe85d9a1846bcdfb01
Griesser Gabriel <gabriel.griesser@he-arc.ch>
2018-03-13 15:05:45 +0100

    Add 'update password'

--- beerreviews/users/urls.py
+++ beerreviews/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', auth_views.login, name='login'),
     path('logout/', users_views.signout, name='logout'),
     path('signup/', users_views.signup, name='signup'),
+    url(r'^password/$', users_views.change_password, name='change_password'),
     url(r'^profile$', users_views.profile, name='profile'),
 ]
--- beerreviews/users/views.py
+++ beerreviews/users/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 import beerreviews
 
 # sign up
@@ -28,6 +30,23 @@ def signout(request):
     return redirect('/')
 
 
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'profile/change_password.html', {
+        'form': form
+    })
+
+
 @login_required
 def profile(request):
-    return render(request, 'profile/profile.html')
+    return render(request, 'profile/profile.html', {'user': request.user})
